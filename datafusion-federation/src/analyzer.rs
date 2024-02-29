b6097b2d9c12c0a57659653b7872e1319d357567
rajantikare <rajan.tikare@esoptra.com>
2024-02-29 19:58:14 +0530

    chore: dummy changes

--- datafusion-federation/src/analyzer.rs
+++ datafusion-federation/src/analyzer.rs
@@ -150,7 +150,7 @@ fn wrap_projection(plan: LogicalPlan) -> Result<LogicalPlan> {
 }
 
 pub fn get_table_source(source: Arc<dyn TableSource>) -> Result<Arc<dyn FederatedTableSource>> {
-    // Unwrap TableSource
+    // Unwrap TableSource.
     let source = source_as_provider(&source)?;
 
     // Get FederatedTableProviderAdaptor
